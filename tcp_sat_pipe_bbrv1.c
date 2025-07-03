db7b03f4b3e621ce3fc5799e82b2e8ddfe1f2d02
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2025-07-04 06:56:16 +0900

    change module name.

--- tcp_sat_pipe_bbrv1.c
+++ tcp_sat_pipe_bbrv1.c
@@ -1202,7 +1202,7 @@ __bpf_kfunc static void bbr_set_state(struct sock *sk, u8 new_state)
 
 static struct tcp_congestion_ops tcp_bbr_cong_ops __read_mostly = {
 	.flags		= TCP_CONG_NON_RESTRICTED,
-	.name		= "bbr",
+	.name		= "sat_pipe_bbr",
 	.owner		= THIS_MODULE,
 	.init		= bbr_init,
 	.cong_control	= bbr_main,
