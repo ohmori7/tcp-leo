379adb1aa8cc396adae39e5da60cbea186f56207
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2026-01-18 11:14:01 +0900

    fix the bug that did not properly use LEO debug flag.

--- tcp_leo.h
+++ tcp_leo.h
@@ -1,7 +1,7 @@
 #ifdef LEO_NODEBUG
 #define DP(...)
 #else /* LEO_NODEBUG */
-bool leo_debug __read_mostly;
+extern bool leo_debug __read_mostly;
 #define DP(...)	if (leo_debug) printk(__VA_ARGS__)
 #endif /* ! LEO_NODEBUG */
 
