581db3533c0ab8ac861182d10101686b627b1526
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2026-09-20 21:03:52 +0900

    hide TCP LEO data structure.

--- tcp_leo.c
+++ tcp_leo.c
@@ -5,6 +5,20 @@
 
 #include "tcp_leo.h"
 
+/*
+ * We could not hold hrtimer here in struct bictcp because of
+ * build failure of sizeof(struct bictcp) > ICSK_CA_PRIV_SIZE.
+ * In addition, hrtimer may be difficult to handle with socket
+ * for locking.
+ * In case BBR, sizeof(struct bbr) == ICSK_CA_PRIVE_SIZE,
+ * and there is no space available.
+ */
+struct leo {
+	struct timer_list handover_timer;
+	void *sock;
+	u32 *last_snd_cwnd;
+};
+
 bool leo_debug __read_mostly = false;
 EXPORT_SYMBOL(leo_debug);
 
